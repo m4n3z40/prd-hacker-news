394e4688af2e3098274fddf4a75b707b98cf15ca
Allan Baptista <allan.marques@ymail.com>
2024-05-01 13:35:48 -0300

    Projeto node criado (package.json) e exeplo de depuração

---
+++ slugify.js
@@ -0,0 +1,16 @@
+function createSlug(str) {
+  str = str.toLowerCase();
+  str = str.replace(/[^\w\s-]/g, '');
+  str = str.replace(/\s+/g, '-');
+  str = str.replace(/--+/g, '-');
+  str = str.trim();
+
+  return str;
+}
+
+const args = process.argv.slice(2).join(' ');
+
+console.log(createSlug(args));
+
+// Hacker News - Agora para produteiros
+// hacker-news-agora-para-produteiros
