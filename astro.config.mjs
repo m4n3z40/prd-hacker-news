495f367225582e395f2aabc2402e902b1b697fdf
Allan Baptista <allan@beanimus.com>
2024-09-16 00:50:56 -0300

    Adding slugs and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 import awsAmplify from 'astro-aws-amplify';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: process.env.URL || 'http://localhost:4321',
   output: 'server',
+
   adapter: process.env.NODE_ENV === 'production'
     ? awsAmplify()
-    : (await import('@astrojs/node')).default({ mode: "standalone" })
+    : (await import('@astrojs/node')).default({ mode: "standalone" }),
+
+  integrations: [sitemap()]
 });
